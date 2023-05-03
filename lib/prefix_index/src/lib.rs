c7475ab732425c7ca703729634d65947c545b048
Matt Gabrenya <matt@buildyourweb.app>
2023-05-03 15:37:51 -0700

    chore: file rename for clarity

--- lib/prefix_index/src/lib.rs
+++ lib/prefix_index/src/lib.rs
@@ -3,7 +3,7 @@
 //! A crate for prefix indexing of labelled hashes
 //!
 //! Useful for type-ahead "search" or autocomplete features.
-pub mod prefix_index_to_hashes;
-pub use prefix_index_to_hashes::*;
+pub mod prefix_index;
+pub use prefix_index::*;
 mod validate;
 mod utils;
