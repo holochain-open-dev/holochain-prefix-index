f5d593184f494ccaa7b432aca70a8e0c6c1223d5
Jost Schulte <jost.schulte@protonmail.com>
2023-07-17 15:44:37 -0600

    build: upgrade to holochain 0.3.0-beta-dev.9

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     holochain = {
       url = "github:holochain/holochain";
       inputs.versions.follows = "versions";
-      inputs.holochain.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.8";
+      inputs.holochain.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.9";
     };
   };
 
